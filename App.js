222a1af265bff0acc46547aa779ebe290d923d77
Anmar Hamad <anmar.a.hamad@gmail.com>
2022-07-01 15:28:19 +0400

    update app

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+
+
+      <View style={{
+        flex: 1,
+        width: '100%',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'flex-start',
+        flexGrow: 1,
+        flexShrink: 1,
+        backgroundColor: 'yellow'
+      }}>
+        <View style={{
+          flex: 1,
+          width: '100%',
+          height: '10%',
+          flexGrow: 0.5,
+          backgroundColor: 'black',
+          
+
+        }} >
+          <Text style={{ color: 'white', fontSize: 25, alignItems: 'center', justifyContent: 'center'}}>Welcome to Animator-x!!</Text>
+        </View>
+      </View>
+      <View style={{
+        flex: 1,
+        width: '100%',
+        height: 90,
+        flexGrow: 1,
+        backgroundColor: 'blue'
+      }} />
+      <View style={{
+        flex: 1,
+        width: '100%',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+        backgroundColor: 'green'
+      }}>
+        <View style={{
+          flex: 1,
+          width: 150,
+          height: 60,
+          paddingTop: 0,
+          marginVertical: 10,
+          backgroundColor: 'pink'
+        }} />
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
