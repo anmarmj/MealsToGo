98fe640df173b09743eff21672767b2936d72f61
Anmar Hamad <anmar.a.hamad@gmail.com>
2022-07-06 13:28:34 +0400

    theming and consistancy

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar as ExpStatusBar } from "expo-status-bar";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import ResturantsScreen from "./src/features/resturants/screens/resturants.screen";
+import { ThemeProvider } from "styled-components/native";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <ResturantsScreen />
-      <ExpStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
