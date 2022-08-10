adf13109ba73e11fe390b5231c1341cf92deb774
Anmar Hamad <anmar.a.hamad@gmail.com>
2022-08-10 11:00:46 +0400

    update loader

--- src/components/loader/loader.styls.js
+++ src/components/loader/loader.styls.js
@@ -1,12 +1,13 @@
 import styled from "styled-components/native";
-import { ActivityIndicator } from "react-native";
 
 export const LoaderContainer = styled.View`
-  position: absolute;
-  top: 50%;
-  left: 50%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
 `;
 
-export const Loading = styled(ActivityIndicator)`
-  margin-left: -25px;
+export const Loading = styled.Image`
+  width: 80px;
+  height: 80px;
 `;
